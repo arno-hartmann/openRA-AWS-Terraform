c64d21740378b2e62f72e531f34399db9ccf7126
arno-hartmann <arno.philipp.hartmann@gmail.com>
2024-04-04 17:27:17 +0200

    Init variable cidr-subnet

--- variables.tf
+++ variables.tf
@@ -7,6 +7,11 @@ variable "cidr-vpc" {
   type        = string
   description = "Cidr Block of VPC"
 }
+
+variable "cidr-subnet" {
+  type        = string
+  description = "Cidr Block of Subnet"
+}
 variable "ressource_name_tag" {
   type        = string
   description = "tags naming the ressources (disguise if wanted)"
