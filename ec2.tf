89e1c4f7d67287bfa29511c929927a7ab11c71e9
arno-hartmann <arno.philipp.hartmann@gmail.com>
2024-04-04 17:38:46 +0200

    Init ec2

---
+++ ec2.tf
@@ -0,0 +1,18 @@
+
+resource "aws_instance" "openredalert_instance" {
+  ami           = data.aws_ami.ubuntu-linux-2204.id
+  instance_type = var.instance_type
+
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.openredalert_subnet.id
+  vpc_security_group_ids      = [aws_security_group.openredalert_sg.id]
+  user_data                   = data.template_file.openra_user_data.rendered
+
+  tags = {
+    Name = "${var.ressource_name_tag}Server"
+  }
+}
+
+output "open_ra_pub_ip" {
+  value = aws_instance.openredalert_instance.public_ip
+}
