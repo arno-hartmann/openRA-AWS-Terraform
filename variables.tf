a2d62d5343bd0440ed3bd47b684f590994c86ddd
arno-hartmann <arno.philipp.hartmann@gmail.com>
2024-04-04 17:25:41 +0200

    Init variable cidr-vpc

--- variables.tf
+++ variables.tf
@@ -3,6 +3,10 @@ variable "aws_profile" {
   description = "used aws proifle (see credentials)"
 }
 
+variable "cidr-vpc" {
+  type        = string
+  description = "Cidr Block of VPC"
+}
 variable "ressource_name_tag" {
   type        = string
   description = "tags naming the ressources (disguise if wanted)"
